8fe9984705cdbe586f6ff7219073d1e86152e5a2
ariqhikari <ahidayatbia@gmail.com>
2022-12-30 20:30:41 +0700

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameOver;
+
+    public GameObject[] stars;
+
+    public int collectedStars;
+
+    public GameObject panel;
+    public GameObject objective;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+        isGameOver = false;
+        collectedStars = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collectedStars == stars.Length)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {   
+        isGameOver = true;
+        panel.SetActive(true);
+        foreach(var star in stars)
+        {
+            star.SetActive(false);
+        }
+        objective.SetActive(false);
+    }
+
+    public void DisplayStars()
+    {
+        for (int i = 0; i < collectedStars; i++)
+        {
+            stars[i].GetComponent<Image>().color = Color.white;
+        }
+    }
+}
